877f35d1db6d86105ec083729be0dcc8d38e11ce
JiunJieLin <sl10066116@gmail.com>
2023-11-09 18:08:04 +0800

    stepBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import ContactDetails from "./components/ContactDetails";
+import StepBar from "./components/StepBar";
 const App = () => {
   return (
     <div className="flex w-full items-center justify-center">
       <div className="b-[1px] m-auto flex flex-col items-center justify-between gap-[31px]">
         <div className="rounded-md pb-[80px]  pl-[46px] pr-[56px] shadow-md">
-          <div>prograss Bar</div>
+          <div className="flex items-center justify-center gap-[134px] py-[33px] ">
+            <div className="flex h-[34px] w-[34px] items-center justify-center rounded-full bg-primary text-white">
+              1
+            </div>
+            <div className="flex h-[34px] w-[34px] items-center justify-center rounded-full bg-[#EFF0F6] text-[#6F6C90]">
+              2
+            </div>
+            <div className="flex h-[34px] w-[34px] items-center justify-center rounded-full bg-[#EFF0F6] text-[#6F6C90]">
+              3
+            </div>
+            <div className="flex h-[34px] w-[34px] items-center justify-center rounded-full bg-[#EFF0F6] text-[#6F6C90]">
+              4
+            </div>
+          </div>
           <div className="h-[1px] w-[596px] bg-[#D9DBE9]"></div>
           <ContactDetails />
         </div>
