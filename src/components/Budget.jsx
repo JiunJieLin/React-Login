e4417147b1a3c5f5a436ca29c6a86f60ebb70b12
JiunJieLin <sl10066116@gmail.com>
2023-11-10 20:26:53 +0800

    UI prev

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,5 +1,41 @@
+import circle from "../public/pictures/circle.png";
 const Budget = () => {
-  return <div>123</div>;
+  return (
+    <div className=" mt-[64px]">
+      <h1 className="mb-[7.5px] text-[24px] font-bold ">
+        What's your project budget?
+      </h1>
+      <p className="mb-[39px]">
+        Lorem ipsum dolor sit amet consectetur adipisicing.
+      </p>
+      <div className="grid grid-cols-2 gap-[44px]">
+        <div className="flex h-[115px] w-[284px] items-center gap-[12px]  rounded-2xl border-[1px] py-[24px] pl-[32px]">
+          <div className="flex items-center justify-center rounded-full py-[45px] ">
+            <img src={circle} alt="" />
+          </div>
+          <p>$5.000 - $10.000</p>
+        </div>
+        <div className="flex h-[115px] w-[284px] items-center gap-[12px]  rounded-2xl border-[1px] py-[24px] pl-[32px]">
+          <div className="flex items-center justify-center rounded-full py-[45px] ">
+            <img src={circle} alt="" />
+          </div>
+          <p>$5.000 - $10.000</p>
+        </div>
+        <div className="flex h-[115px] w-[284px] items-center gap-[12px]  rounded-2xl border-[1px] py-[24px] pl-[32px]">
+          <div className="flex items-center justify-center rounded-full py-[45px] ">
+            <img src={circle} alt="" />
+          </div>
+          <p>$5.000 - $10.000</p>
+        </div>
+        <div className="flex h-[115px] w-[284px] items-center gap-[12px]  rounded-2xl border-[1px] py-[24px] pl-[32px]">
+          <div className="flex items-center justify-center rounded-full py-[45px] ">
+            <img src={circle} alt="" />
+          </div>
+          <p>$5.000 - $10.000</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Budget;
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,5 +1,26 @@
 const Summary = () => {
-  return <div>123</div>;
+  return (
+    <div className=" mt-[64px]">
+      <h1 className="mb-[7.5px] text-[24px] font-bold ">Summary</h1>
+      <p className="mb-[39px]">
+        Lorem ipsum dolor sit amet consectetur adipisicing.
+      </p>
+      <div className="flex flex-col items-center border-2">
+        <div>
+          <p>Name:</p>
+          <p>Phone:</p>
+          <p>Email:</p>
+          <p>Company:</p>
+          <p>Our Service:</p>
+          <p>Budget:</p>
+        </div>
+
+        <button className="rounded-full bg-primary px-[40px] py-[20px] text-white">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Summary;
