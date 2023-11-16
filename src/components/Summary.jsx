9d4860af1d2081f4e3f0bce43b80835a2258b7b9
JiunJieLin <sl10066116@gmail.com>
2023-11-16 11:52:54 +0800

    Summary style

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,18 +1,26 @@
 const Summary = ({ step1Contact, step2Contact, step3Contact }) => {
+  const summaryDatas = [
+    ["Name:", step1Contact.name],
+    ["Cell Phone:", step1Contact.phone],
+    ["Email:", step1Contact.email],
+    ["Company:", step1Contact.company],
+    ["Our Service:", step2Contact],
+    ["Budget:", step3Contact],
+  ];
   return (
     <div className=" mt-[64px]">
       <h1 className="mb-[7.5px] text-[24px] font-bold ">Summary</h1>
       <p className="mb-[39px]">
         Lorem ipsum dolor sit amet consectetur adipisicing.
       </p>
-      <div className="flex flex-col items-center border-2">
-        <div>
-          <p>Name:{step1Contact.name}</p>
-          <p>Phone:{step1Contact.phone}</p>
-          <p>Email:{step1Contact.email}</p>
-          <p>Company:{step1Contact.company}</p>
-          <p>Our Service:{step2Contact}</p>
-          <p>Budget:${step3Contact}</p>
+      <div className="flex flex-col items-center gap-8 border-2 p-3">
+        <div className="flex flex-col gap-2 ">
+          {summaryDatas.map((data, index) => (
+            <p key={index} className="flex items-start ">
+              <span className="w-[120px] font-bold">{data[0]}</span>
+              {data[1]}
+            </p>
+          ))}
         </div>
 
         <button className="rounded-full bg-primary px-[40px] py-[20px] text-white">
